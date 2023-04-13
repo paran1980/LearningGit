9a205b97364867e8e16258c30fa612b16396c6e5
Parandhaman Margan <paran1980@gmail.com>
2023-04-13 23:58:52 +0530

    Feature changes to DevTest

--- src/test/scala/DevTest.scala
+++ src/test/scala/DevTest.scala
@@ -2,11 +2,35 @@ package org.paran.learningGit
 
 
 class DevTest {
-  def printSomething (msg: String): Unit =
-  {
+  def printSomething (msg: String): Unit = {
+
     println {
       s"hello world $msg"
     }
     // paran added this
+
+
+    println {
+      s"hello world "
+    }
+    // paran added this
+
+
+    println {
+      s"hello world "
+    }
+    // paran added this
+
+
+    println {
+      s"hello world "
+    }
+    // paran added this
+    println {
+      s"hello world "
+    }
+    // paran added this
+
+
   }
 }
