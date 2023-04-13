6514be341fcc9c11b8d35ca0632182e6b4775c92
paran1980 <43312377+paran1980@users.noreply.github.com>
2023-04-13 23:53:31 +0530

    from origin: New change
    New Change from master

--- src/main/scala/DevClass.scala
+++ src/main/scala/DevClass.scala
@@ -4,5 +4,6 @@ class DevClass {
   def printSomething(msg:String): Unit = {
     println("hello world - BharathiParan-123")
     //checkin from BharathiParan
+    println("new change from master")
   }
 }
