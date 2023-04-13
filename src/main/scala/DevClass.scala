2169b333cf76a7bb88b32fb6c9cb2875d1dc725f
BharathiParan <130691270+BharathiParan@users.noreply.github.com>
2023-04-13 23:14:16 +0530

    Update DevClass.scala
    Added more changes

--- src/main/scala/DevClass.scala
+++ src/main/scala/DevClass.scala
@@ -2,7 +2,7 @@ package org.paran.learningGit
 
 class DevClass {
   def printSomething(msg:String): Unit = {
-    println("hello world - BharathiParan")
+    println("hello world - BharathiParan-123")
     //checkin from BharathiParan
   }
 }
