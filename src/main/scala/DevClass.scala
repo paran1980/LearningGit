e058a43d72639b164c926ed78efaf4efb75279ff
BharathiParan <130691270+BharathiParan@users.noreply.github.com>
2023-04-14 00:15:49 +0530

    Update DevClass.scala (#4)

--- src/main/scala/DevClass.scala
+++ src/main/scala/DevClass.scala
@@ -5,5 +5,6 @@ class DevClass {
     println("hello world - BharathiParan-123")
     //checkin from BharathiParan
     println("new change from master")
+    println("DevClass-BharathiParan-456")
   }
 }
