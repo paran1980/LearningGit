9b17bff12e478cc75c71ad5b18589a6933edaa0a
paran1980 <43312377+paran1980@users.noreply.github.com>
2023-04-14 12:43:39 +0530

    change from feature pull request (#9)
    * Changes from master and feature branch
    
    * Feature changes After Rebase 2nd change
    
    * changes to devclass from master branch
    
    changes to devclass from master branch
    
    * Changes from master and feature branch
    
    * Feature changes to devclass file---1
    
    * Feature changes to devclass file---2

--- src/main/scala/DevClass.scala
+++ src/main/scala/DevClass.scala
@@ -15,10 +15,24 @@ class DevClass {
     println("Changes from master branch1----2")
     println("changes from feature1 branch")
     println("2nd change from feature1 branch")
-    
+
+
     ///
     ///
     println("new change from master---3")
     println("new chage from master branch---4")
+
+    println("changes from feature1 branch")
+
+    //---
+
+    println("change from feature branch --3")
+    println("change from feature branch --4")
+
+    println("change to feature branch --5")
+    println("change to feature branch -6")
+
+    //--
+
   }
 }
